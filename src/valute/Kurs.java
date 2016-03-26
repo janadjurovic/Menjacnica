d29ae9edeabc502de09e54aca459dc0fb43e4191
janadjurovic <djurovic.jana@outlook.com>
2016-03-26 20:43:56 +0100

    Dodate metode toString(), equals() i hashCode() u obje klase

--- src/valute/Kurs.java
+++ src/valute/Kurs.java
@@ -32,6 +32,36 @@ public class Kurs {
 	public void setKupovniKurs(double kupovniKurs) {
 		this.kupovniKurs = kupovniKurs;
 	}
+	@Override
+	public String toString() {
+		return "Kurs [datum=" + datum + ", prodajniKurs=" + prodajniKurs + ", srednjiKurs=" + srednjiKurs
+				+ ", kupovniKurs=" + kupovniKurs + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(prodajniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(srednjiKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (obj != null && obj instanceof Kurs){
+			Kurs k = (Kurs) (obj);
+			if(k.getProdajniKurs() == prodajniKurs && k.getKupovniKurs() == kupovniKurs 
+					&& k.getSrednjiKurs() == srednjiKurs && k.getDatum().equals(datum)){
+				return true;
+			}
+		} 
+		return false;
+	}
 	
 	
 }
--- src/valute/Valuta.java
+++ src/valute/Valuta.java
@@ -23,6 +23,30 @@ public class Valuta {
 	public void setKurs(Kurs kurs) {
 		this.kurs = kurs;
 	}
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceniNaziv=" + skraceniNaziv + ", kurs=" + kurs + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kurs == null) ? 0 : kurs.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(obj != null && obj instanceof Valuta){
+			Valuta v = (Valuta) (obj);
+			if(v.getKurs().equals(kurs) && v.getNaziv().equals(naziv) 
+					&& v.getSkraceniNaziv().equals(skraceniNaziv)){
+				return true;
+			}
+		}
+		return false;
+	}
 	
 	
 }
