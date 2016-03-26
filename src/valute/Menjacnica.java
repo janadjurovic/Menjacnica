e57264f7ad67522feeda04a089d948d3160e71c2
janadjurovic <djurovic.jana@outlook.com>
2016-03-26 21:14:01 +0100

    Na grani 'implementacijainterfejsa' implementirane metode klase Menjacnica koja implementira MenjacnicaInterfejs

--- src/valute/Menjacnica.java
+++ src/valute/Menjacnica.java
@@ -10,19 +10,37 @@ public class Menjacnica implements MenjacnicaInterfejs {
 	@Override
 	public void dodajKurs(LinkedList<Kurs> kursevi, GregorianCalendar datum, double prodajniKurs, double srednjiKurs,
 			double kupovniKurs) {
-		// TODO Auto-generated method stub
+		
+		Kurs k = new Kurs();
+		k.setDatum(datum);
+		k.setProdajniKurs(prodajniKurs);
+		k.setSrednjiKurs(srednjiKurs);
+		k.setKupovniKurs(kupovniKurs);
+		
+		kursevi.add(k);
 
 	}
 
 	@Override
 	public void obrisiKurs(LinkedList<Kurs> kursevi, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		
+		for(int i = 0; i < kursevi.size(); i++){
+			if(kursevi.get(i).getDatum().equals(datum)){
+				kursevi.remove(i);
+			}
+		}
 
 	}
 
 	@Override
 	public Kurs pronadjiKurs(LinkedList<Kurs> kursevi, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		
+		for(int i = 0; i < kursevi.size(); i++){
+			if(kursevi.get(i).getDatum().equals(datum)){
+				return kursevi.get(i);
+			}
+		}
+		
 		return null;
 	}
 
