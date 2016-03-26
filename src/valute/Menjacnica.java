c2ed4fea853a8c715ac6299144817ea86e6ead44
janadjurovic <djurovic.jana@outlook.com>
2016-03-26 21:06:49 +0100

    Kreirana klasa Menjacnica koja implementira MenjacnicaInterfejs

---
+++ src/valute/Menjacnica.java
@@ -0,0 +1,29 @@
+package valute;
+
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+import interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKurs(LinkedList<Kurs> kursevi, GregorianCalendar datum, double prodajniKurs, double srednjiKurs,
+			double kupovniKurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs(LinkedList<Kurs> kursevi, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Kurs pronadjiKurs(LinkedList<Kurs> kursevi, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
